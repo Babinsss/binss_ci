e0f9a057e0351288ff745fe82e820405c0d3a7f7
Binns <vincentbarrientos51@gmail.com>
2024-02-27 11:27:21 +0800

    intl commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'wow_ci_db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
