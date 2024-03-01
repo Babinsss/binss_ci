478371d6d6e61a8127bb6d1c42e1b9bd83f2728a
Binns <vincentbarrientos51@gmail.com>
2024-03-01 10:30:16 +0800

    intl commit

--- app/Views/user/add.php
+++ app/Views/user/add.php
@@ -1,60 +1,61 @@
 <?= $this->extend('layout/main') ?>
 
 <?= $this->section('content') ?>
+<?= $this->extend('layout/main') ?>
+
+<?= $this->section('content') ?>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-6">
-            <div class="card mt-3">
-                <div class="card-body">
-                    <h5 class="card-title text-center">Add User</h5>
-                    <form action="<?= base_url('/storeUser') ?>" method="post">
-                        <?= csrf_field() ?>
-                        <div class="mb-3">
-                            <label for="first_name" class="form-label">First Name</label>
-                            <input type="text" class="form-control" id="first_name" name="first_name" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="middle_name" class="form-label">Middle Name</label>
-                            <input type="text" class="form-control" id="middle_name" name="middle_name">
-                        </div>
-                        <div class="mb-3">
-                            <label for="last_name" class="form-label">Last Name</label>
-                            <input type="text" class="form-control" id="last_name" name="last_name" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="age" class="form-label">Age</label>
-                            <input type="number" class="form-control" id="age" name="age" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="gender_id" class="form-label">Gender</label>
-                            <select class="form-select" id="gender_id" name="gender_id" required>
-                                <option value="" selected disabled>Select Gender</option>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="confirm_password" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
-                        </div>
-                        <div class="d-grid gap-2">
-                            <button type="submit" class="btn btn-primary">Save</button>
-                        </div>
-                    </form>
+    <form action="<?= base_url() ?>storeUser" method="post">
+        <?= csrf_field() ?>
+        <div class="card mt-3">
+            <div class="card-body">
+                <h5 class="card-title">Add User</h5>
+                <div class="mb-3">
+                    <label for="first_name">First Name</label>
+                    <input type="text" class="form-control" id="first_name" name="first_name" />
+                </div>
+                <div class="mb-3">
+                    <label for="middle_name">Middle Name</label>
+                    <input type="text" class="form-control" id="middle_name" name="middle_name" />
+                </div>
+                <div class="mb-3">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" class="form-control" id="last_name" name="last_name" />
+                </div>
+                <div class="mb-3">
+                    <label for="age">Age</label>
+                    <input type="text" class="form-control" id="age" name="age" />
+                </div>
+                <div class="mb-3">
+                    <label for="gender_id">Gender</label>
+                    <select class="form-select" id="gender_id" name="gender_id">
+                        <?php foreach ($genders as $g) : ?>
+                            <option value="<?= $g->gender_id ?>"><?= $g->g ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" id="email" name="email" />
+                </div>
+                <div class="mb-3">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" />
+                </div>
+                <div class="mb-3">
+                    <label for="confirm_password">Confirm Password</label>
+                    <input type="password" class="form-control" id="confirm_password" name="confirm_password" />
+                </div>
+                <div class="col-12 col-sm-3 float-end">
+                    <button type="submit" class="btn btn-success w-100">Save</button>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 </div>
+
+<?= $this->endSection('content') ?>
+
 
 <?= $this->endSection('content') ?>
